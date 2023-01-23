19d4e4f91369783de897b526bb5bc7d555948e6e
Eldar <initmyself@gmail.com>
2023-01-23 23:28:03 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int check = 1200;                       //счет
+        int replenishment = 800;               //сумма пополнения
+        int finalScore = check + replenishment; //финальный счет
+        int bonus = replenishment / 100;        //бонусы
+
+        if (replenishment >= 1100) {
+            System.out.println(finalScore + bonus); }
+        else
+        {
+            System.out.println(finalScore);
+        }
+
     }
 }
