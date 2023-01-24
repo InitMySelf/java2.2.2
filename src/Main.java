3d8bf7242fb10e59a1ad2a24fdf33c123419b6ce
Eldar <initmyself@gmail.com>
2023-01-24 16:12:40 +0300

    Change commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
         int check = 1200;                       //счет
-        int replenishment = 800;               //сумма пополнения
+        int replenishment = 1050;               //сумма пополнения
         int finalScore = check + replenishment; //финальный счет
         int bonus = replenishment / 100;        //бонусы
-
         if (replenishment >= 1100) {
-            System.out.println(finalScore + bonus); }
-        else
-        {
+            System.out.println(finalScore + bonus);
+        } else {
             System.out.println(finalScore);
         }
 
