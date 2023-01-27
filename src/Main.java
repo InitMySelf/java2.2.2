469ee9757c6d3c6ec7d10e5e1523a3ea10b32ff5
Eldar <initmyself@gmail.com>
2023-01-27 23:32:50 +0300

    Change commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int check = 100;
-        int replenishment = 1050;
+        int replenishment = 1001;
         int bonus;
 
-        if (replenishment >= 1000) {
+        if (replenishment > 1000) {
             bonus = replenishment / 100;
         } else {
             bonus = 0;
